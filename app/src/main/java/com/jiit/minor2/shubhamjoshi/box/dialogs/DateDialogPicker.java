f6a7168ab56c78f4c6e813ab72142d1dd4ece080
Shubham Joshi <1shubhamjoshi1@gmail.com>
2016-02-07 00:29:45 +0530

    date and gender functionality added

---
+++ app/src/main/java/com/jiit/minor2/shubhamjoshi/box/dialogs/DateDialogPicker.java
@@ -0,0 +1,46 @@
+package com.jiit.minor2.shubhamjoshi.box.dialogs;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by Shubham Joshi on 06-02-2016.
+ */
+public class DateDialogPicker extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    private TextView dateTextView;
+
+    public DateDialogPicker(View view) {
+        dateTextView = (TextView) view;
+
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int month = c.get(Calendar.MONTH);
+        return new DatePickerDialog(getActivity(), AlertDialog.THEME_HOLO_DARK, this, year, month, day);
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        String date=checkDigit(monthOfYear+1)+"/"+checkDigit(dayOfMonth)+"/"+year;
+        dateTextView.setText(date);
+    }
+
+    public String checkDigit(int number)
+    {
+        return number<=9?"0"+number:String.valueOf(number);
+    }
+
+}
